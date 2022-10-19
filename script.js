e5fc36d96ec469056e8d6c854e21ff67200400ae
Precious Orjiude <indigosoftwares21@gmail.com>
2022-10-19 19:36:06 +0300

    footer

---
+++ script.js
@@ -0,0 +1,12 @@
+const openMenu = ()=>{
+    var navmenu = document.getElementById("mobilenav");
+    navmenu.style.opacity = "1";
+    navmenu.style.pointerEvents = "initial";
+
+}
+const closeMenu = ()=>{
+    var navmenu = document.getElementById("mobilenav");
+    navmenu.style.opacity = "0";
+    navmenu.style.pointerEvents = "none";
+
+}
