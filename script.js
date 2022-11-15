a535f67c32851a4e37234f54c1293202349d684b
Precious Orjiude <indigosoftwares21@gmail.com>
2022-11-15 10:51:36 +0200

    new code

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ const closeMenu = ()=>{
     navmenu.style.pointerEvents = "none";
 
 }
+function github()
+{
+    window.open('https://github.com/IndigoSoftwares21', '_blank')
+}
+function instagram()
+{
+    window.open('https://www.instagram.com/preciousorjiude/?hl=en', '_blank')
+}
+function youtube()
+{
+    window.open('https://www.youtube.com/channel/UC6Pjv-Av4_skmzOlj_cNxsg', '_blank')
+}
+function tiktok()
+{
+    window.open('https://www.tiktok.com/@indigocoder', '_blank')
+}
+function email()
+{
+    window.open('mailto:sullivanorjiude@gmail.com', '_blank')
+}
