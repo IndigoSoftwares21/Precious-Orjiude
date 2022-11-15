6feebb2535ea10c0cb300930088d1a3be7c0e881
Precious Orjiude <indigosoftwares21@gmail.com>
2022-11-15 11:27:59 +0200

    new

--- script.js
+++ script.js
@@ -30,3 +30,15 @@ function email()
 {
     window.open('mailto:sullivanorjiude@gmail.com', '_blank')
 }
+function crackr(){
+    window.open('https://github.com/IndigoSoftwares21/CrackR', '_blank')
+}
+function loggr(){
+    window.open('https://loggrapp.netlify.app/', '_blank')
+}
+function geofinder(){
+    window.open('https://d.apkpure.com/b/APK/com.indigosoftwares.geofinder?version=latest', '_blank')
+}
+function meteo(){
+    window.open('https://meteowe.netlify.app/', '_blank')
+}
