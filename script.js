19f82911e4342874d08c2528175fb3cea3612913
indigosoftwares21 <indigosoftwares21@gmail.com>
2024-04-12 13:28:32 +0300

    Update chat configuration and add download resume functionality

--- script.js
+++ script.js
@@ -1,47 +1,90 @@
-const openMenu = ()=>{
-    var navmenu = document.getElementById("mobilenav");
-    navmenu.style.opacity = "1";
-    navmenu.style.pointerEvents = "initial";
+// window.cmndChat("init", {
+//   hide_default_launcher: false,
+//   custom_launcher_selector: "orjiude-div",
+//   chatbot_id:"1",
+//   organization_id:1,
+// });
 
-}
-const closeMenu = ()=>{
-    var navmenu = document.getElementById("mobilenav");
-    navmenu.style.opacity = "0";
-    navmenu.style.pointerEvents = "none";
+// type StyleConfig = {
+//   userMessageBubbleBgColor?: string;
+//   responseMessageBubbleBgColor?: string;
+//   userMessageBubbleTextColor?: string;
+//   responseMessageBubbleTextColor?: string;
+//   loadingDotColor?: string;
+//   widgetBgColor?: string;
+//   widgetBorderRadius?: string;
+//   WidgetButtonBorderRadius?: string;
+//   widgetButtonBgColor?: string;
+//   widgetButtonIconColor?: string;
+//   widgetButtonClosedIconColor?: string;
+//   widgetButtonOpenedIconColor?: string;
+//   sendIconColor?: string;
+//   sendIconBgColor?: string;
+//   newChatIconcolor?: string;
+//   expandIconColor?: string;
+//   minimiseIconColor?: string;
+//   inputBgColor?: string;
+//   inputBorderColor?: string;
+//   inputBorderRadius?: string;
+//   inputTextColor?: string;
+//   inputTextSize?: string;
+//   inputPlaceholderText?: string;
+//   inputPlaceholderColor?: string;
+//   footerTextColor?: string;
+//   titleFontSize?: string;
+//   subTitleFontSize?: string;
+//   iconSize?: string;
+//   widgetShadowIntensity?: string;
+//   widgetAnimation?: string;
+//   widgetButtonPosition?:"left"| "right";
+// };
 
+
+const openMenu = () => {
+  var navmenu = document.getElementById("mobilenav");
+  navmenu.style.opacity = "1";
+  navmenu.style.pointerEvents = "initial";
+};
+const closeMenu = () => {
+  var navmenu = document.getElementById("mobilenav");
+  navmenu.style.opacity = "0";
+  navmenu.style.pointerEvents = "none";
+};
+function github() {
+  window.open("https://github.com/IndigoSoftwares21", "_blank");
 }
-function github()
-{
-    window.open('https://github.com/IndigoSoftwares21', '_blank')
+function instagram() {
+  window.open("https://www.instagram.com/preciousorjiude/?hl=en", "_blank");
 }
-function instagram()
-{
-    window.open('https://www.instagram.com/preciousorjiude/?hl=en', '_blank')
+function youtube() {
+  window.open(
+    "https://www.youtube.com/channel/UC6Pjv-Av4_skmzOlj_cNxsg",
+    "_blank"
+  );
 }
-function youtube()
-{
-    window.open('https://www.youtube.com/channel/UC6Pjv-Av4_skmzOlj_cNxsg', '_blank')
+function tiktok() {
+  window.open("https://www.tiktok.com/@indigocoder", "_blank");
 }
-function tiktok()
-{
-    window.open('https://www.tiktok.com/@indigocoder', '_blank')
+function email() {
+  window.open("mailto:sullivanorjiude@gmail.com", "_blank");
 }
-function email()
-{
-    window.open('mailto:sullivanorjiude@gmail.com', '_blank')
+function crackr() {
+  window.open("https://github.com/IndigoSoftwares21/CrackR", "_blank");
 }
-function crackr(){
-    window.open('https://github.com/IndigoSoftwares21/CrackR', '_blank')
+function loggr() {
+  window.open("https://loggrapp.netlify.app/", "_blank");
 }
-function loggr(){
-    window.open('https://loggrapp.netlify.app/', '_blank')
+function geofinder() {
+  window.open(
+    "https://d.apkpure.com/b/APK/com.indigosoftwares.geofinder?version=latest",
+    "_blank"
+  );
 }
-function geofinder(){
-    window.open('https://d.apkpure.com/b/APK/com.indigosoftwares.geofinder?version=latest', '_blank')
+function meteo() {
+  window.open("https://meteowe.netlify.app/", "_blank");
 }
-function meteo(){
-    window.open('https://meteowe.netlify.app/', '_blank')
-}
-function resume(){
-    location.href= 'preciousorjiude_resume.pdf'
+function resume() {
+  console.log("hide me");
+ 
+ // location.href = "preciousorjiude_resume.pdf";
 }
